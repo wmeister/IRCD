cdd5527800c2fbdc7b2d4a7ea96072ff716da3c6
william <wmeister86@gmail.com>
2019-09-23 08:47:51 -0400

    clion and cmake

---
+++ cmake-build-debug/CMakeFiles/IRCD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IRCD.dir/IRCD/IRCD/main.cpp.o"
+  "IRCD.pdb"
+  "IRCD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IRCD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
